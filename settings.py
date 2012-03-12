8813889186ec56fc1bf53d92a062d35b0afd4bb2
Tony Angerilli <tony@angerilli.ca>
2012-03-11 20:57:29 -0700

    Moved stuff to the root of the repo

--- settings.py
+++ settings.py
@@ -6,10 +6,11 @@ HTMLDIR = os.path.join(CURRENTDIR, "static", "html")
 TRELLO_API_URL = "https://api.trello.com/1/"
 BOARD_ID = "" # The ID of the board to track (find it in the URL of your board)
 
+# TODO: Could build something into the app to guide the user through generating these values
 API_KEY = "" # Your trello api key (from https://trello.com/1/appKey/generate)
 USER_TOKEN = "" # See http://www.trello.org/help.html ("Getting a Token from a User")
 
 NOT_DONE_LISTS = ("To Do", "Doing")
 DONE_LISTS = ("Done")
 
-DATABASE_URI = "sqlite:///%s" % os.path.join(CURRENTDIR, "data.db")
+DATABASE_URI = "sqlite:///%s" % os.path.join(CURRENTDIR, "spark.db")
